b790b5da4c7ff417d04016bd10e208ab6be8f370
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2024-12-18 09:31:34 +0700

    fixing route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SocialiteController;
+use App\Http\Controllers\Auth\SocialiteController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
