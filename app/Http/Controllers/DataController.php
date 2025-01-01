0324c22ab3b037899e398b711803c9b040cfa3b8
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2025-01-02 01:04:56 +0700

    changing payload name

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -16,7 +16,7 @@ class DataController extends Controller
     public function index()
     {
         $data = Data::with('user')->paginate(10); // Eager load related user
-        return response()->json(['data' => $data, 'status'=>"Success"], 200);
+        return response()->json(['article_data' => $data, 'status'=>"Success"], 200);
     }
 
     /**
